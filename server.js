a5de384579de68063f5e2e6e22641eceecea19dc
Cody Samuels <twowoodenspoons@gmail.com>
2020-08-28 10:24:51 -0700

    enabled cors for real

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const app = express();
-// const cors = require("cors")
+const cors = require("cors")
 const PORT = process.env.PORT || 3001;
 require('dotenv').config()
 
